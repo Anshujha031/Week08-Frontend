86861aa442edd42638d29017d98e4b7de829fbbc
naveenjha031 <naveenjha344@gmail.com>
2025-03-04 12:25:38 +0530

    [Naveen] Add : Display Weeks Problem

---
+++ Day01-(JsProgrammingConstructor)/If-else/DisplayWeek.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+// Get user input
+const number = parseInt(prompt("Enter a number (1-7): "));
+
+
+if (number === 1) console.log("Sunday");
+else if (number === 2) console.log("Monday");
+else if (number === 3) console.log("Tuesday");
+else if (number === 4) console.log("Wednesday");
+else if (number === 5) console.log("Thursday");
+else if (number === 6) console.log("Friday");
+else if (number === 7) console.log("Saturday");
+else console.log("Invalid input! Please enter a number between 1 and 7.");
