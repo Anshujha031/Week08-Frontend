30e7f881feaa3bab8ae631ceead43b36ded232f0
naveenjha031 <naveenjha344@gmail.com>
2025-03-04 12:30:47 +0530

    [Naveen] Add : Read Single Digit Problem

---
+++ Day01-(JsProgrammingConstructor)/CaseStatement/ReadSingleDigit.js
@@ -0,0 +1,40 @@
+const prompt = require('prompt-sync')();
+
+// Get user input
+const digit = parseInt(prompt("Enter a single digit number (0-9): "));
+
+// Convert digit to word using switch case
+switch (digit) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    default:
+        console.log("Invalid input! Please enter a single digit number (0-9).");
+}
