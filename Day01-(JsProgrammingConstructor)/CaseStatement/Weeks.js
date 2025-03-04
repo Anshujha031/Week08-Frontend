8b0877efb7f16ec76be8fbc723b331c07c5c1545
naveenjha031 <naveenjha344@gmail.com>
2025-03-04 12:32:38 +0530

    [Naveen] Add : Display Weeks Problem

---
+++ Day01-(JsProgrammingConstructor)/CaseStatement/Weeks.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+// Get user input
+const number = parseInt(prompt("Enter a number (1-7): "));
+
+// Convert number to weekday using switch case
+switch (number) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid input! Please enter a number between 1 and 7.");
+}
