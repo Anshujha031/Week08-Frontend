5431cd30f6d1ec23f9bedc263abacf5c59260265
naveenjha031 <naveenjha344@gmail.com>
2025-03-04 13:15:34 +0530

    [Naveen] Add : Print Table Problem

---
+++ Day01-(JsProgrammingConstructor)/forloop/whileloop/Printtable.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+
+const n = parseInt(prompt("Enter a number (n): "));
+
+
+let i = 0;
+let power = 1;
+console.log("Powers of 2 up to 2^" + n + " (till 256):");
+while (i <= n && power <= 256) {
+    console.log("2^" + i + " = " + power);
+    power *= 2;
+    i++;
+}
