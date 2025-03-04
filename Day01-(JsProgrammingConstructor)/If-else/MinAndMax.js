f6e4859e48d46a36e35bdb773d0f7173a1155293
naveenjha031 <naveenjha344@gmail.com>
2025-03-04 12:21:09 +0530

    [Naveen] Add : Min Max Problem

---
+++ Day01-(JsProgrammingConstructor)/If-else/MinAndMax.js
@@ -0,0 +1,9 @@
+let numbers = Array.from({ length: 5 }, () => Math.floor(Math.random() * 900) + 100);
+
+
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
+
+console.log("Generated Numbers:", numbers);
+console.log("Minimum Value:", min);
+console.log("Maximum Value:", max);
